5348785a8b81fd8cf0ea788f015c91eb9e442acf
Yasmina Bruton <student.yasmina.bruton@tricountytech.edu>
2022-11-14 17:23:28 -0600

    finish project 5

--- 05-Project-1/calculator.py
+++ 05-Project-1/calculator.py
@@ -1,3 +1,10 @@
-ABT = input("ABT")
-print(ABT * 0.15, ABT * 0.20, ABT * 0.25)
-input("AG")
+moneysb4tip = float(input())
+numguests = int(input())
+if (numguests) >= 8:
+    print ("A 20% tip is automatically added to parties of 8 or more.")
+    TipOptions8 = "Your bill is {}"
+    print(TipOptions8.format(round(moneysb4tip * 1.2, 2)))
+else:
+    print ("Your tip options are:")
+    TipOptions = "15%: {} 20%: {} 25% {}"
+    print(TipOptions.format(round(moneysb4tip * 1.15, 2), round(moneysb4tip * 1.20, 2), round(moneysb4tip * 1.25, 2)))
